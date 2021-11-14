106c5e36e9e5bc6c3f8e639c848069f18158e38f
JoaoMarinho <61231873+JoaoAMarinho@users.noreply.github.com>
2021-11-14 19:33:21 +0000

    footer and login

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ const header=document.querySelector('header')
 document.addEventListener('scroll',()=>{
     var scroll_position = window.scrollY;
     if(scroll_position>230){
-        header.style.backgroundColor = "#C4C4C4"
+        header.style.backgroundColor = "#6F2232"
     }else {
         header.style.backgroundColor = "transparent"
     }
