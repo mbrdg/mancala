c08cdb2a5a90ce912db043e4112e3ab1353c7662
JoaoMarinho <61231873+JoaoAMarinho@users.noreply.github.com>
2021-11-14 14:45:31 +0000

    Adds some style and js file

---
+++ src/app.js
@@ -0,0 +1,36 @@
+//Hamburguer & Menu Animation
+const hamburguer=document.querySelector('header nav .nav-list .hamburguer');
+const menu=document.querySelector('header nav .nav-list ul');
+const menu_item=document.querySelectorAll('header nav .nav-list ul li a');
+
+hamburguer.addEventListener('click',() => {
+    hamburguer.classList.toggle('active');
+    if(menu.classList.contains('active')){
+        menu.classList.remove('active');
+        menu.classList.toggle('inactive');
+    } 
+    else {
+        menu.classList.remove('inactive');
+        menu.classList.toggle('active');
+    }
+});
+
+menu_item.forEach((item) => {
+    item.addEventListener('click',()=>{
+        hamburguer.classList.toggle('active');
+        menu.classList.remove('active');
+        menu.classList.toggle('inactive');
+    })
+});
+
+//Header background-change
+const header=document.querySelector('header')
+
+document.addEventListener('scroll',()=>{
+    var scroll_position = window.scrollY;
+    if(scroll_position>230){
+        header.style.backgroundColor = "#C4C4C4"
+    }else {
+        header.style.backgroundColor = "transparent"
+    }
+});
