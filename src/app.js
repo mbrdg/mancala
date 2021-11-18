93216ac76a2a76f01d6a5a5de12b0346ade5004c
JoaoMarinho <61231873+JoaoAMarinho@users.noreply.github.com>
2021-11-18 19:44:45 +0000

    a

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
+const body = document.body;
+
 //Hamburguer & Menu Animation
 const hamburguer=document.querySelector('header nav .nav-list .hamburguer');
 const menu=document.querySelector('header nav .nav-list ul');
-const menu_item=document.querySelectorAll('header nav .nav-list ul li a');
+const menuItem=document.querySelectorAll('header nav .nav-list ul li a');
 
 hamburguer.addEventListener('click',() => {
     hamburguer.classList.toggle('active');
@@ -15,7 +17,7 @@ hamburguer.addEventListener('click',() => {
     }
 });
 
-menu_item.forEach((item) => {
+menuItem.forEach((item) => {
     item.addEventListener('click',()=>{
         hamburguer.classList.toggle('active');
         menu.classList.remove('active');
@@ -29,8 +31,29 @@ const header=document.querySelector('header')
 document.addEventListener('scroll',()=>{
     var scroll_position = window.scrollY;
     if(scroll_position>230){
-        //header.style.backgroundColor = "#6F2232"
+        header.style.backgroundColor = "#6F2232"
     }else {
         header.style.backgroundColor = "transparent"
     }
+});
+
+//Play
+const play = document.querySelector('#play');
+const welcomeMenu = document.querySelector('#play .welcome-menu');
+const moreSettingsMenu = document.querySelector('#play .more-settings');
+
+const moreSettingsBtn = document.querySelector('#play .welcome-menu .settings form #more-settings-btn');
+const closeBtn = document.querySelector('#play .more-settings .close-settings');
+console.log(closeBtn);
+
+moreSettingsBtn.addEventListener('click',() => {
+    play.scrollIntoView();
+    welcomeMenu.classList.toggle('active');
+    moreSettingsMenu.classList.toggle('active');
+    body.style.overflow = 'hidden';
+});
+closeBtn.addEventListener('click',() => {
+    welcomeMenu.classList.toggle('active');
+    moreSettingsMenu.classList.toggle('active');
+    body.style.overflow = 'visible';
 });
