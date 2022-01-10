03e1d4be15215f2e84a412ef9cd99e3dd99dee24
JoaoMarinho <61231873+JoaoAMarinho@users.noreply.github.com>
2022-01-10 23:35:58 +0000

    Start server implementation

--- index.js
+++ index.js
@@ -1,12 +1,90 @@
 const http = require('http');
-const fs = require('fs');
 const url  = require('url');
 
-const PORT = 3000;
+const fs = require('fs');
 
-const server = http.createServer(function(req, res) {
-    const parsedUrl = url.parse(req.url,true);
-    const pathname = parsedUrl.pathname;
+const updater = require('.updater.js');
+//Modulo de game
+//Modulo de autenticação
+//...
+const PORT = 3000;
+/*
+module.exports.algo = function(){} || value
+const mod = require('./module.js');
+mod.algo();
+*/
+/* Preferencia assíncrona (bellow)
+
+fs.readFile('file', function(error, data) {
+    if (error) return;
+
+})
+fs.writeFile('file', (error) => {
+    if (error) return;
+    
+})
+*/
+
+
+const headers = {
+    plain: {
+        'Content-Type': 'application/javascript',
+        'Cache-Control': 'no-cache',
+        'Access-Control-Allow-Origin': '*'        
+    },
+    sse: {    
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
+        'Access-Control-Allow-Origin': '*',
+        'Connection': 'keep-alive'
+    }
+};
+
+const server = http.createServer(function(request, response) {
+    const parsedUrl = url.parse(request.url,true);
+    const pathName = parsedUrl.pathname;
+    let answer = {};
+
+    if (request.method === 'POST') {
+        switch(pathName) {
+            case '/register':
+                break;
+            case '/join':
+                break;
+            case '/leave':
+                break;
+            case '/notify':
+                break;
+            case '/ranking':
+                break;
+            default:
+                answer.status = 400;
+                break;
+        }
+    } 
+    else if (request.method === 'GET'){
+        switch(pathName) {
+            case '/update':
+                updater.remember(response);
+                answer.style = 'sse';
+                break;
+            default:
+                answer.status = 400;
+                break;
+        }
+    }
+    else {
+        answer.status = 400;
+    }
+
+    if(answer.status === undefined)
+        answer.status = 200;
+    if(answer.style === undefined)
+        answer.style = 'plain';
+
+    response.writeHead(answer.status, headers[answer.style]);
+    if(answer.style === 'plain')
+        response.end();
 })
 
 server.listen(PORT, function(error) {
